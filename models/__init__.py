9f2af7d5c4b880a06cd30ccce0136fc1dadadf72
xioperez01 <angie.xioperez@gmail.com>
2020-11-04 06:30:06 +0000

    FileStorage

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Init """
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
---
+++ models/engine/file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import json
+from models.base_model import BaseModel
+""" Class File_storage """
+
+
+class FileStorage():
+    """ Serializes instances to a JSON file and
+    deserializes JSON file to instances """
+
+    # Private class attributes:
+    __file_path = "file.json"
+    __objects = {}
+
+    # Public instance methods
+    def all(self):
+        """ returns the dictionary __objects """
+        return self.__objects
+
+    def new(self, obj):
+        """ Sets in __objects the new obj with
+        key <obj class name>.id """
+        if obj:
+            k = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects[k] = obj
+
+    def save(self):
+        """ Serializes __objects to the JSON file
+        (path: __file_path)"""
+        dictionary = {}
+        for k, obj in self.__objects.items():
+            dictionary[k] = obj.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(dictionary, f)
+
+    def reload(self):
+        """ If json file exists, convert obj dicts back to instances"""
+        try:
+            with open(FileStorage.__file_path) as fp:
+                data = json.load(fp)
+        except FileNotFoundError:
+            return
+        for key, value in data.items():
+            eval(key.split(".")[0] + '(**value)')
