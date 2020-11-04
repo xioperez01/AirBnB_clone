96c03b9f6d31e4f16629eb20d4ab3dfc3740ba0d
Angie Pérez <66028978+xioperez01@users.noreply.github.com>
2020-11-04 17:38:12 -0500

    Update console.py

--- console.py
+++ console.py
@@ -12,9 +12,9 @@ class HBNBCommand(cmd.Cmd):
     of the command interpreter """
     file = None
     if sys.stdin.isatty():
-        """intro = "*****************************\n*\
+        intro = "*****************************\n*\
                            *\n*       Welcome :)          *\n*\
-                           *\n*****************************\n""""
+                           *\n*****************************\n"
         prompt = "(hbnb)"
     else:
         prompt = "(hbnb)\n"
